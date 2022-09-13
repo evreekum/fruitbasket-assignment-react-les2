0eb6560a228d39f524702564e7e0e84c4cdaaa9b
evreekum <e.v.reekum@gmail.com>
2022-09-13 12:31:51 +0200

    boilerplate and started assignment with Buttons component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,67 @@
 import React from 'react';
 import './App.css';
+import Buttons from "./components/Buttons";
+
 
 function App() {
-  return (
-    <>
-      <h1>Fruitmand bezorgservice</h1>
-    </>
-  );
+
+    // const plusCounter = () => setCounter(counter + 1);
+    // const minCounter = () => setCounter(counter - 1);
+    // if (counter <= 0) {
+    //     minCounter = () => setCounter(0);
+    // }
+
+    return (
+        <>
+            <h1>Fruitmand bezorgservice</h1>
+
+            <Buttons
+                fruitname="Aardbeien"
+            />
+
+         {/*   <div className="fruit-container">
+                <h2>Aardbeien</h2>
+                <button
+                    type="button"
+                    onClick={() => setCounter(counter - 1)}
+                    disabled={counter === 0}
+                >
+                    -
+                </button>
+                {counter}
+                <button
+                    type="button"
+                    onClick={() => setCounter(counter + 1)}
+                >
+                    +
+                </button>
+            </div>
+
+            <div className="fruit-container">
+                <h2>Aardbeien</h2>
+                <button
+                    type="button"
+                    onClick={() => setCounter(counter - 1)}
+                    disabled={counter === 0}
+                >
+                    -
+                </button>
+                {counter}
+                <button
+                    type="button"
+                    onClick={() => setCounter(counter + 1)}
+                >
+                    +
+                </button>
+
+
+            </div>*/}
+
+
+
+            {/*<ButtonPlus onClickFunc={plusCounter}/>*/}
+        </>
+    );
 }
 
 export default App;
